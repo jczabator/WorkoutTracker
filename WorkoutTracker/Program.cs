d83918da4a47204c068050bd198eab16833580e4
DESKTOP-T8GU483\jczab <jczabator@gmail.com>
2022-01-06 00:58:02 +0100

    Change .NET version to 6.0

--- WorkoutTracker/Program.cs
+++ WorkoutTracker/Program.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using WorkoutTracker.Data;
 
 namespace WorkoutTracker
 {
-  public class Program
+    public class Program
   {
     public static void Main(string[] args)
     {
