0140baca780c76cb186ffe9164dfe54b27b0f8ef
DESKTOP-T8GU483\jczab <jczabator@gmail.com>
2022-01-14 00:33:08 +0100

    Update training values

--- WorkoutTracker/Data/DbInitializer.cs
+++ WorkoutTracker/Data/DbInitializer.cs
@@ -44,22 +44,22 @@ namespace WorkoutTracker.Data
 
       var workoutProgresses = new WorkoutProgress[]
       {
-        new() {Id = 1, UserId = 1, ExerciseId = 1, Repetitions = 6, Sets = 4, Weight = 35, EquipmentType = EquipmentType.BigBarbell},
-        new() {Id = 2, UserId = 1, ExerciseId = 2, Repetitions = 6, Sets = 4, Weight = 7.5m, EquipmentType = EquipmentType.BigBarbell},
-        new() {Id = 3, UserId = 1, ExerciseId = 3, Repetitions = 6, Sets = 4, Weight = 10, EquipmentType = EquipmentType.BigBarbell},
-        new() {Id = 4, UserId = 1, ExerciseId = 4, Repetitions = 7, Sets = 4, Weight = 40, EquipmentType = EquipmentType.SmallBarbell},
+        new() {Id = 1, UserId = 1, ExerciseId = 1, Repetitions = 6, Sets = 4, Weight = 40, EquipmentType = EquipmentType.BigBarbell},
+        new() {Id = 2, UserId = 1, ExerciseId = 2, Repetitions = 6, Sets = 4, Weight = 10, EquipmentType = EquipmentType.BigBarbell},
+        new() {Id = 3, UserId = 1, ExerciseId = 3, Repetitions = 6, Sets = 4, Weight = 11.5m, EquipmentType = EquipmentType.BigBarbell},
+        new() {Id = 4, UserId = 1, ExerciseId = 4, Repetitions = 7, Sets = 4, Weight = 45, EquipmentType = EquipmentType.SmallBarbell},
         new() {Id = 5, UserId = 1, ExerciseId = 5, Repetitions = 8, Sets = 4, Weight = 10, EquipmentType = EquipmentType.Dumbbell},
         new() {Id = 6, UserId = 1, ExerciseId = 5, Repetitions = 6, Sets = 4, Weight = 20, EquipmentType = EquipmentType.SmallBarbell},
-        new() {Id = 7, UserId = 1, ExerciseId = 13, Repetitions = 6, Sets = 4, Weight = 10, EquipmentType = EquipmentType.BigBarbell},
+        new() {Id = 7, UserId = 1, ExerciseId = 13, Repetitions = 6, Sets = 4, Weight = 12.5m, EquipmentType = EquipmentType.BigBarbell},
         new() {Id = 8, UserId = 1, ExerciseId = 6, Repetitions = 6, Sets = 4, Weight = 20, EquipmentType = EquipmentType.SmallBarbell},
-        new() {Id = 9, UserId = 1, ExerciseId = 7, Repetitions = 12, Sets = 4, Weight = 25, EquipmentType = EquipmentType.Machine},
-        new() {Id = 10, UserId = 1, ExerciseId = 13, Repetitions = 6, Sets = 4, Weight = 17.5m, EquipmentType = EquipmentType.Dumbbell},
-        new() {Id = 11, UserId = 1, ExerciseId = 1, Repetitions = 6, Sets = 4, Weight = 20, EquipmentType = EquipmentType.Machine},
+        new() {Id = 9, UserId = 1, ExerciseId = 7, Repetitions = 12, Sets = 4, Weight = 35, EquipmentType = EquipmentType.Machine},
+        new() {Id = 10, UserId = 1, ExerciseId = 13, Repetitions = 6, Sets = 4, Weight = 20, EquipmentType = EquipmentType.Dumbbell},
+        new() {Id = 11, UserId = 1, ExerciseId = 1, Repetitions = 6, Sets = 4, Weight = 35, EquipmentType = EquipmentType.Machine},
         new() {Id = 12, UserId = 1, ExerciseId = 8, Repetitions = 6, Sets = 4, Weight = 20, EquipmentType = EquipmentType.Machine},
-        new() {Id = 13, UserId = 1, ExerciseId = 9, Repetitions = 6, Sets = 4, Weight = 20, EquipmentType = EquipmentType.Machine},
+        new() {Id = 13, UserId = 1, ExerciseId = 9, Repetitions = 6, Sets = 4, Weight = 15, EquipmentType = EquipmentType.Machine},
         new() {Id = 14, UserId = 1, ExerciseId = 3, Repetitions = 12, Sets = 4, Weight = 25, EquipmentType = EquipmentType.Sandbag},
         new() {Id = 15, UserId = 1, ExerciseId = 10, Repetitions = 1, Sets = 3, Weight = 0, EquipmentType = EquipmentType.None, TempoTraining = "1-60-x-1"},
-        new() {Id = 16, UserId = 1, ExerciseId = 11, Repetitions = 6, Sets = 4, Weight = 20, EquipmentType = EquipmentType.SmallBarbell},
+        new() {Id = 16, UserId = 1, ExerciseId = 11, Repetitions = 6, Sets = 4, Weight = 25, EquipmentType = EquipmentType.SmallBarbell},
         new() {Id = 17, UserId = 1, ExerciseId = 12, Repetitions = 17, Sets = 5, Weight = 0, EquipmentType = EquipmentType.None}
       };
 
